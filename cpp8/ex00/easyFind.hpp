ada917177ff35b7b456b39ee3c5b2f7fd4cfe0ba
Antoine BEDIN <antoinebedin@orange.fr>
2025-04-14 09:51:45 +0200

    .

--- cpp8/ex00/easyFind.hpp
+++ cpp8/ex00/easyFind.hpp
@@ -7,16 +7,73 @@
 // 		\ sequence containers : list, vector, deque, (array), (forward list)
 // 		\ associative containers : set, multiset, map, multimap, (the same ones, unordered)
 // 		\ container adapters : stack, queue, priority queue
-// -> function <easyFind> manages only list/vecto/deque,
+// -> function <easyFind> manages only list/vector/deque,
 // 	  since the subject excludes associative containers, and container adapters have no iterators
 
-template <class TContainer>
-int*	easyFind(TContainer container, int value)
-{
-	int*	location;
 
-	location = reinterpret_cast<int *>(std::find(container.begin(), container.end(), value));
-	return (location);
+template <template<class, class> class C, class T, class A>
+typename C<T,A>::iterator	easyFind(C<T,A>& container, T value)
+{
+	typename C<T,A>::iterator	it;
+
+	it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		std::cout << "value " << value << " not found\n";
+	else
+		std::cout << "value " << value << " found as " << *it << "\n";
+	return (it);
 }
+
+template <template<class, class> class C, class T, class A>
+void	displayContainer(C<T, A >& container)
+{
+	int							ind = 0;
+	typename C<T,A>::iterator	it;
+
+	for (it = container.begin(); it != container.end(); it++)
+	{
+		std::cout << "pos " << ind << " : " << *it << "\n";
+		ind++;
+	}
+}
+
+/*
+template <template<class, class> class C, class T, template <class> class A>
+void	displayContainer(C<T, A<T> >& container)
+{
+	int		ind;
+	T		elem;
+
+	for (ind = 0; ind < 3; ind++)
+	{
+		elem = container[ind];
+		std::cout << "pos " << ind << " : " << elem << "\n";
+	}
+}
+*/
+
+/*
+template <template <class> class C, class T>
+void	displayContainer(C<T>& container)
+{
+	int		ind;
+
+	for (ind = 0; ind < 3; ind++)
+	{
+		std::cout << "pos " << ind << " : " << container[ind] << "\n";
+	}
+}
+
+template <class C>
+void	displayContainer(C& container)
+{
+	int		ind;
+
+	for (ind = 0; ind < 3; ind++)
+	{
+		std::cout << "pos " << ind << " : " << container[ind] << "\n";
+	}
+}
+*/
 
 #endif
--- cpp8/ex00/main.cpp
+++ cpp8/ex00/main.cpp
@@ -1,18 +1,31 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 #include <algorithm>
 #include "easyFind.hpp"
 
 int	main(void)
 {
-	std::vector<int>			v;
-	std::vector<int>::iterator	it;
-	int*						location;
+	std::vector<int>									v;
+	std::list<int>										l;
+	std::deque<int>										d;
+	std::vector<int>::iterator							itv;
+	std::list<int>::iterator							itl;
+	std::deque<int>::iterator							itd;
 
 	v.push_back(6);
 	v.push_back(12);
 	v.push_back(24);
+	l.push_back(3);
+	l.push_back(4);
+	l.push_back(5);
+	d.push_back(20);
+	d.push_back(30);
+	d.push_back(40);
+	(void)itv;
+	(void)itl;
 
 	/*
 	std::cout << "at pos 1 : " << v[1] << "\n";
@@ -22,11 +35,28 @@ int	main(void)
 	std::cout << "result of find = pos " << (it - v.begin()) << ", content " << *it << "\n";
 	*/
 
+	
+	displayContainer(v);
 	std::cout << "at pos 1 : " << v[1] << "\n";
-	location = easyFind(v, 12);
-	std::cout << "result of find = pos " << (location) << ", content " << *location << "\n";
+	itv = easyFind(v, 12);
+	std::cout << "result of find = pos " << (itv - v.begin()) << ", content " << *itv << "\n";
 	v[1] = 8;
-	std::cout << "result of find = pos " << (location) << ", content " << *location << "\n";
+	std::cout << "result of find = pos " << (itv - v.begin()) << ", content " << *itv << "\n";
+	
+	displayContainer(l);
+	std::cout << "at pos 0 : " << l.front() << "\n";
+	itl = easyFind(l, 3);
+	std::cout << "result of find - content " << *itl << "\n";
+	l.front() = 1;
+	std::cout << "result of find - content " << *itl << "\n";
+
+	displayContainer(d);
+	std::cout << "at pos 0 : " << d[2] << "\n";
+	itd = easyFind(d, 40);
+	std::cout << "result of find - content " << *itd << "\n";
+	d[2] = 100;
+	std::cout << "result of find - content " << *itd << "\n";
+
 
 	return 0;
 }
